958222c21d99bff0d15a2f5c95a9b1562127b2d2
willy836 <wilsonkimiri836@gmail.com>
2023-04-29 17:11:57 +0300

    Complete project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import data from "./components/data";
+import List from "./components/List";
+import React, { useState } from "react";
 
 function App() {
+  const [people, setPeople] = useState(data);
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button className="btn" onClick={() => setPeople([])}>
+          clear list
+        </button>
+      </section>
+    </main>
   );
 }
 
